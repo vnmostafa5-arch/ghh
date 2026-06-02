d19528fd55df001a1041ba852b89035056bfcf5c
vnmostafa5-arch <vnmostafa5@gmail.com>
2026-06-02 15:59:53 +0300

    Add files via upload

---
+++ world-timer-mod/src/client/java/com/vnmostafa5/worldtimer/client/WorldTimerClient.java
@@ -0,0 +1,36 @@
+package com.vnmostafa5.worldtimer.client;
+
+import com.vnmostafa5.worldtimer.TimeTracker;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+
+public class WorldTimerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        HudRenderCallback.EVENT.register(this::renderTimerHUD);
+    }
+
+    private void renderTimerHUD(GuiGraphics drawContext, float tickDelta) {
+        Minecraft client = Minecraft.getInstance();
+
+        if (client.player == null || client.options.hideGui) {
+            return;
+        }
+
+        TimeTracker tracker = TimeTracker.getInstance();
+        if (!tracker.isRunning()) {
+            return;
+        }
+
+        String timeText = tracker.getFormattedTotalTime();
+        Font font = client.font;
+
+        // Shadow for readability
+        drawContext.drawString(font, timeText, 6, 6, 0x000000);
+        // Gold text
+        drawContext.drawString(font, timeText, 5, 5, 0xFFD700);
+    }
+}
